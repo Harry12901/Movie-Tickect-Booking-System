1a16b04974ad79892c6036facbc0d0de159b2c1a
Harry12901 <harinisivaes@gmail.com>
2022-05-08 10:05:48 +0530

    Updation

---
+++ tickets/aboutus.php
@@ -0,0 +1,77 @@
+<?php include('header.php'); ?>
+
+<html>
+<head>
+<title>Online Tickets</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+<link type="text/css" rel="stylesheet" href="http://www.dreamtemplate.com/dreamcodes/tabs/css/tsc_tabs.css" />
+<link rel="stylesheet" href="css/tsc_tabs.css" type="text/css" media="all" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='js/jquery.color-RGBa-patch.js'></script>
+<script src='js/example.js'></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class="content"> 
+		<div class="wrap">
+		<h1 style="text-align: center;">
+			Description
+		</h1>
+		<p style="text-align: center;">
+			This Site focuses on allowing the end-users to book movie tickets online in advance, know the different show timing, watch the movie trailer, and read reviews for the same. The Ticket Reservation System is a web application software/portal that can be accessed by anyone connected to the internet.
+		</p>
+
+<!-- 
+		<table cellpadding="2" cellspacing="2">
+			<tr>
+				<th><b>S.NO</b></th>
+				<th><b>Name</b></th>
+				<th><b>Roll Number</b></th>
+			</tr>
+		</table> -->
+		<center>
+
+		<table style="width:40%" cellspacing="2" border="5px solid ">
+  <tr>
+    <th><b>S.NO</b></th>
+    <th><b>NAME</b></th>
+    <th><b>ROLL NUMBER</b></th>
+  </tr>
+   <tr>
+    <td>1</td>
+    <td><a href="https://www.linkedin.com/in/chintala-rohitkrishnasai-689820196/" target="_blank">CH ROHITKRISHNASAI</a></td>
+    <td>CB.EN.U4CSE19013</td>
+  </tr>
+   <tr >
+    <td>2</td>
+    <td ><a href="https://www.linkedin.com/in/lalith-varma-746439199/" target="_blank">D LALITH</a></td>
+    <td>CB.EN.U4CSE19014</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td><a href="https://www.linkedin.com/in/harini-sivakumar-039450163/" target="_blank">HARINI S</a></td>
+    <td>CB.EN.U4CSE19023</td>
+  </tr>
+   <tr>
+    <td>4</td>
+    <td><a href="https://www.linkedin.com/in/vinaya-karumuru-82a702217/" target="_blank">VINAYA</a></td>
+    <td>CB.EN.U4CSE19028</td>
+  </tr>
+   <tr>
+    <td>5</td>
+    <td><a href="https://www.linkedin.com/in/nithish-janagamoorthi-a726151a3/" target="_blank">NITHISH</a></td>
+    <td>CB.EN.U4CSE19039</td>
+  </tr>
+  
+</table>
+</center>
+	</div>
+
+</div>
+</body>
+
+<?php include('footer.php');?>
